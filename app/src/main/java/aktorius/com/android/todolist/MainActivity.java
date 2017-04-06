9d91e975c5dc388d6f26dade333cc31fb2d5c1f3
Aktorius <miri.tarik@gmail.com>
2017-04-06 20:58:24 +0100

    basic layout implementation

--- app/src/main/java/aktorius/com/android/todolist/MainActivity.java
+++ app/src/main/java/aktorius/com/android/todolist/MainActivity.java
@@ -1,7 +1,12 @@
 package aktorius.com.android.todolist;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.rvMainActivity);
+
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+        mRecyclerView.setHasFixedSize(true);
+
+        FloatingActionButton fabActionButton = (FloatingActionButton)findViewById(R.id.fabAddTask);
+        fabActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO add
+            }
+        });
     }
 }
